a1006d03253a40cc8cfb403d3e9a7b8fc8f2f517
luisa moncayo <luisa_15092000@hotmail.com>
2019-11-29 15:50:37 -0500

    clases agregadas

---
+++ Parcial2/Parcial2/src/View/Main.java
@@ -0,0 +1,32 @@
+package View;
+
+import processing.core.PApplet;
+
+public class Main extends PApplet{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		PApplet.main(Main.class.getName());
+
+	}
+	
+	
+	public void setup() {
+		
+	}
+	
+	public void settings() {
+		
+	}
+	
+	public void draw() {
+		
+	}
+	
+	public void mousePressed() {
+		
+	}
+	
+	
+
+}
